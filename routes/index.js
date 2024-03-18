f614cb14978a0c045e496dcdd15dbf6407b676e1
rohitrv7 <rohitkumar115032002@gmail.com>
2024-03-18 15:54:58 +0530

    Added mongoose to connect to a MongoDB database and created a user schema and model. Added routes for about, services, and contact pages. Added a form to the contact page to collect user information and save it to the database.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
+const { connect } = require('mongoose');
+const userModel = require('./users')
 var router = express.Router();
 
+
+// const mongoose = connect.mongoose("")
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/about', function(req, res, next){
+  res.render('notpage')
+});
+router.get('/services', function(req, res, next){
+  res.render('notpage')
+});
+router.get('/contact', function(req, res, next){
+  res.render('notpage')
+});
+
+
+router.post('/', (req,res,next)=>{
+  const { fullname, email, mobileno, subject, message } = req.body;
+  const userData = new userModel({ fullname, email, mobileno, subject, message });
+  let createUser = userModel.create(userData);
+  res.render('thanks');
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
+const { default: mongoose } = require('mongoose');
 var router = express.Router();
+
+mongoose.connect('mongodb://localhost:27017/users')
+
+const userSchema = new mongoose.Schema({
+  fullname: {
+    type:String
+  },
+  email :{
+    type: String,
+    require: true,
+    unique:true
+  },
+  mobileno :{
+    type: Number,
+    require: true
+  },
+  subject :{
+    type: String
+  },
+  message: {
+    type: String
+  },
+})
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-module.exports = router;
+module.exports = mongoose.model("User",userSchema);
